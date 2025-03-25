60ecf34cb2405ef4446953444eeb8ea5c4371ff3
Artem Dragunov <dragunovartem99@gmail.com>
2025-03-25 21:33:48 +0300

    Add wget

--- consume_csv.sh
+++ consume_csv.sh
@@ -2,5 +2,7 @@
 
 # https://database.lichess.org/#puzzles
 
+# wget https://database.lichess.org/lichess_db_puzzle.csv.zst
+
 mongoimport --db puzzfinder --collection puzzles --type csv --headerline \
 	--file <(zstdcat lichess_db_puzzle.csv.zst | sed '1s/PuzzleId/_id/')
