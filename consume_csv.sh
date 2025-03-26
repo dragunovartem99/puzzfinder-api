e05fe0b1da64674bae2067bd89b4ec3d243f28d2
Artem Dragunov <dragunovartem99@gmail.com>
2025-03-26 13:56:33 +0300

    Drop collection before importing

--- consume_csv.sh
+++ consume_csv.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 # https://database.lichess.org/#puzzles
-
 # wget https://database.lichess.org/lichess_db_puzzle.csv.zst
 
-mongoimport --db puzzfinder --collection puzzles --type csv --headerline \
-	--mode=upsert \
-	--file <(zstdcat lichess_db_puzzle.csv.zst | sed '1s/PuzzleId/_id/')
+mongosh puzzfinder --eval "db.puzzles.drop()"
+mongoimport \
+	--db puzzfinder \
+	--collection puzzles \
+	--type csv \
+	--file <(zstdcat lichess_db_puzzle.csv.zst | sed '1s/PuzzleId/_id/') \
+	--headerline \
+	--mode=upsert
