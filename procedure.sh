897e37399ee3a8fe4c16f62d1d719dbbe4fd9545
Artem Dragunov <dragunovartem99@gmail.com>
2025-03-27 16:26:13 +0300

    More context in procedure comments

--- procedure.sh
+++ procedure.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-# wget https://database.lichess.org/lichess_db_puzzle.csv.zst
-# zstd -d lichess_db_puzzle.csv.zst
+#wget https://database.lichess.org/lichess_db_puzzle.csv.zst
+#zstd -d lichess_db_puzzle.csv.zst
 
 DB_NAME="puzzfinder.db"
 SCHEMA_FILE="sql/schema.sql"
@@ -15,8 +15,9 @@ sqlite3 $DB_NAME < $SCHEMA_FILE
 echo "Importing data... (this might take a while)"
 time sqlite3 $DB_NAME < $IMPORT_FILE
 
-echo -e "\nDatabase setup completed"
-
+# CPU: AMD Ryzen 7 5700U with Radeon Graphics (16) @ 1.800GHz
 # real    4m27.689s
 # user    3m49.763s
 # sys     0m37.507s
+
+echo -e "\nDatabase setup completed"
