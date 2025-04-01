8ae835843d6adf5ccfa7af283e81135aef9cb870
Artem Dragunov <dragunovartem99@gmail.com>
2025-04-01 16:25:55 +0300

    Use boolean flags approach

--- create_database.sh
+++ create_database.sh
@@ -3,25 +3,71 @@
 #wget https://database.lichess.org/lichess_db_puzzle.csv.zst
 #zstd -d lichess_db_puzzle.csv.zst
 
-DB_NAME="puzzfinder.db"
+source ./themes
+
+INPUT_CSV="lichess_db_puzzle.csv"
+DB_FILE="puzzfinder.db"
 SCHEMA_FILE="sql/schema.sql"
-IMPORT_FILE="sql/import_data.sql"
-INDEXES_FILE="sql/indexes.sql"
 
-echo "Starting database setup"
-echo "Creating schema..."
+# Generate the schema with theme columns
+{
+    echo "CREATE TABLE puzzles ("
+    echo "    id TEXT PRIMARY KEY,"
+    echo "    fen TEXT NOT NULL,"
+    echo "    moves TEXT NOT NULL,"
+    echo "    rating INTEGER NOT NULL,"
+    echo "    ratingDeviation INTEGER NOT NULL,"
+    echo "    popularity INTEGER NOT NULL,"
+    echo "    nbPlays INTEGER NOT NULL,"
+    echo "    gameUrl TEXT NOT NULL,"
+    echo "    openingTags TEXT,"
+    echo
+    echo "    -- Jesus, forgive my wicked soul"
 
-sqlite3 $DB_NAME < $SCHEMA_FILE
+    for (( i=0; i<${#THEMES[@]}; i++)); do
+        theme="${THEMES[$i]}"
+        theme="${theme^}"
+        if [ $i -eq $((${#THEMES[@]} - 1)) ]; then
+            echo "    theme${theme} BOOLEAN NOT NULL CHECK (theme${theme} IN (0, 1))"
+        else
+            echo "    theme${theme} BOOLEAN NOT NULL CHECK (theme${theme} IN (0, 1)),"
+        fi
+    done
 
-echo "Importing data... (this will take a while)"
-time sqlite3 $DB_NAME < $IMPORT_FILE
+    echo ");"
+} > "$SCHEMA_FILE"
 
-# CPU: AMD Ryzen 7 5700U with Radeon Graphics (16) @ 1.800GHz
-# real    3m53.243s
-# user    3m43.581s
-# sys     0m9.263s
+sqlite3 $DB_FILE < $SCHEMA_FILE
 
-echo -e "\nCreating indexes"
-time sqlite3 $DB_NAME < $INDEXES_FILE
+# Generate the SQL for importing data with theme columns
+IMPORT_SQL="INSERT INTO puzzles SELECT 
+    id,
+    fen,
+    moves,
+    rating,
+    ratingDeviation,
+    popularity,
+    nbPlays,
+    gameUrl,
+    openingTags,"
 
-echo -e "\nDatabase setup completed"
+for theme in "${THEMES[@]}"; do
+    IMPORT_SQL+="
+    CASE WHEN themes LIKE '%${theme}%' THEN 1 ELSE 0 END AS theme${theme^},"
+done
+
+# Remove trailing comma
+IMPORT_SQL="${IMPORT_SQL%,} FROM temp_import;"
+
+echo "Importing data..."
+sqlite3 "$DB_FILE" <<EOF
+.mode csv
+.import "$INPUT_CSV" temp_import
+
+$IMPORT_SQL
+
+DROP TABLE temp_import;
+VACUUM;
+EOF
+
+echo "Import completed. Database file: $DB_FILE"
