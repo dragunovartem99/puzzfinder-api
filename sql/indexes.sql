b8be6bf504be76b666af57ca39663709ee617ef8
Artem Dragunov <dragunovartem99@gmail.com>
2025-04-01 17:06:10 +0300

    Create indexes for movesNumber

--- sql/indexes.sql
+++ sql/indexes.sql
@@ -1,6 +1,5 @@
 CREATE INDEX idx_puzzles_rating ON puzzles(rating);
+CREATE INDEX idx_puzzles_movesNumber ON puzzles(movesNumber);
 
 CREATE INDEX idx_puzzles_popularity ON puzzles(popularity);
 CREATE INDEX idx_puzzles_nbPlays ON puzzles(nbPlays);
-
--- CREATE INDEX idx_puzzles_movesNumber ON puzzles(movesNumber);
