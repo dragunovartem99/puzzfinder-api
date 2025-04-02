57c53f32f4982ed74c01f686daf3333084606dfe
Artem Dragunov <dragunovartem99@gmail.com>
2025-04-02 23:50:23 +0300

    Fix the bug

--- functions/parseSortOptions.js
+++ functions/parseSortOptions.js
@@ -1,8 +1,8 @@
 export function parseSortOptions(sort) {
-	const validColumns = ["id", "movesNumber", "rating", "popularity", "nbPlays"];
+	const validColumns = ["puzzleId", "movesNumber", "rating", "popularity", "nbPlays"];
 	const validOrders = ["asc", "desc"];
 
-	const sortBy = validColumns.includes(sort.field) ? sort.field : "id";
+	const sortBy = validColumns.includes(sort.field) ? sort.field : "puzzleId";
 	const sortOrder = validOrders.includes(sort.order) ? sort.order : "asc";
 
 	return { by: sortBy, order: sortOrder };
