13b8ab76a57102352614da16249c4ee43f863ffe
Artem Dragunov <dragunovartem99@gmail.com>
2025-04-03 12:15:38 +0300

    Decouple API and DB

--- components/database.js
+++ components/database.js
@@ -1,6 +1,6 @@
 import Database from "better-sqlite3";
 
-const database = new Database("puzzfinder.db");
+const database = new Database("db/puzzfinder.db");
 database.pragma("journal_mode = WAL");
 
 export { database };
