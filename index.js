142f2aeeeb28d3badb7f8e3b1b4dc1f1209c6e58
Artem Dragunov <dragunovartem99@gmail.com>
2025-05-12 17:50:45 +0300

    Add basic error handling

--- index.js
+++ index.js
@@ -4,6 +4,11 @@ import { databaseSearch } from "./functions/databaseSearch.js";
 server.listen(50000, () => console.log(`Puzzfinder API listening on port 50000`));
 
 server.post("/puzzles", async (req, res) => {
-	const { data, pagination } = databaseSearch(req.body);
-	res.json({ data, pagination });
+	try {
+		const { data, pagination } = databaseSearch(req.body);
+		res.json({ data, pagination });
+	} catch (e) {
+		console.error("Search failed:", error);
+		res.status(500).json({ error: "Internal server error" });
+	}
 });
