7b77f80cd758f1e69bd6aaf5a5384080f459b4f3
Artem Dragunov <dragunovartem99@gmail.com>
2026-06-27 20:41:27 +0300

    Rename puzzfinder.caddy to Caddyfile

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 
-cp puzzfinder.caddy /etc/caddy/sites/puzzfinder.caddy
+cp Caddyfile /etc/caddy/sites/puzzfinder.caddy
 systemctl reload caddy
 docker compose up -d --build
 docker image prune -f
